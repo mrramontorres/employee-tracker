5c4c9151853980f3dc257daae0aea5821458c4f7
turtles <me@ramon.nyc>
2021-02-08 22:07:15 -0800

    added prompt for viewing data.

--- tracker.js
+++ tracker.js
@@ -2,6 +2,8 @@
 // =============================================================
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require("console.table");
+const Choices = require("inquirer/lib/objects/choices");
 
 // Connect to the database
 // =============================================================
@@ -18,6 +20,42 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    connection.end();
-  });
-  
+    startApp();
+});
+
+// Initiate prompt
+// =============================================================
+function startApp() {
+  inquirer
+    .prompt({
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments.",
+            "View all roles.",
+            "View all employees.",
+            "Exit."
+        ]
+    })
+    .then(function(answer) {
+        switch (answer.action) {
+
+          case "View all departments.":
+          console.log("1st");
+          break;
+
+          case "View all roles.":
+          console.log("2nd");
+          break;
+
+          case "View all employees.":
+          console.log("3rd");
+          break;
+
+          case "Exit.":
+          connection.end();
+          break;
+        }
+      });
+};
